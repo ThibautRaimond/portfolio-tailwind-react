df633f7fc0e7d5727dcf6ba01f514c1c774dfd21
Thibaut <raimond.thibaut@gmail.com>
2022-11-02 10:15:08 +0100

    profil+experiences

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,21 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
     return (
-        <div className='w-full h-screen bg-[black]'>
+        <div name='home' className='w-full h-screen bg-[#0e1111]'>
             
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-3xl text-[#07ed66]'>Bonjour je suis</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Thibaut Raimond</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#656a79] py-4'>Développer web & mobile</h2>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#07ed66] hover:border-[#07ed66] hover:text-#0e1111]'> Voir mon travail
+                    <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3'/> 
+                    </span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
   
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[black] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0e1111] text-gray-300'>
     <div>
     <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
     </div>
@@ -31,7 +31,7 @@ const Navbar = () => {
     </div>
     
     {/* mobile menu */}
-    <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[black] flex flex-col justify-center items-center'}>
+    <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0e1111] flex flex-col justify-center items-center'}>
     <li className='py-6 text-4xl'>Accueil</li>
     <li className='py-6 text-4xl'>Profil</li>
     <li className='py-6 text-4xl'>Compétences</li>
@@ -43,25 +43,25 @@ const Navbar = () => {
     {/* Social icons*/}
     <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
     <ul>
-    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#0A66C2] '>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0A66C2] '>
     <a className='flex justify-between items-center w-full text-gray-300'
     href="/">
     Linkedin <FaLinkedin size={30} />
     </a>
     </li>
-    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#2d333b] '>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2d333b] '>
     <a className='flex justify-between items-center w-full text-gray-300'
     href="/">
     Github <FaGithub size={30} />
     </a>
     </li>
-    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#ea4335]'>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ea4335]'>
     <a className='flex justify-between items-center w-full text-gray-300'
     href="/">
     Email <HiOutlineMail size={30} />
     </a>
     </li>
-    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#347d39] '>
+    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#347d39] '>
     <a className='flex justify-between items-center w-full text-gray-300'
     href="/">
      Resume <BsFillPersonLinesFill size={30} />
