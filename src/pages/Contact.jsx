348aa953acf6b5900f38fd0e26cfdc51ef2e30b5
Thibaut <raimond.thibaut@gmail.com>
2022-11-09 17:02:40 +0100

    ,

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 						Me contacter
 					</p>
 					<p className="text-gray-300 py-4">
-						Afin de me contacter remplissez le formulaire ci-dessous ou envoyez
+						Afin de me contacter, remplissez le formulaire ci-dessous ou envoyez
 						moi directement un e-mail à l'adresse suivante:
 						raimond.thibaut@gmail.com
 					</p>
