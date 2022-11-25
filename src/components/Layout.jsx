3c0e38ad84dc9a8d829b02caa6e598bce6b65a80
Thibaut <raimond.thibaut@gmail.com>
2022-11-25 15:00:38 +0100

    DesktopSocialIcons component created

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import DesktopSocialIcons from "./DesktopSocialIcons";
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Navbar />
+			<DesktopSocialIcons />
 			<main>{children}</main>
 		</>
 	);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Logo from "../assets/logo.png";
 
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
 import { AiOutlineProfile } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
@@ -160,53 +159,6 @@ const Navbar = () => {
 					</li>
 				</div>
 			</ul>
-
-			{/* ----------------------- Social icons ----------------------- */}
-			<div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-				<ul>
-					<li
-						className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0A66C2] "
-					>
-						<a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/thibaut-raimond-0a46791ab/"
-						target="_blank"
-						rel="noreferrer">
-							Linkedin <FaLinkedin size={30} />
-						</a>
-					</li>
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2d333b] ">
-						<a
-							className="flex justify-between items-center w-full text-gray-300"
-							href="https://github.com/ThibautRaimond"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Github <FaGithub size={30} />
-						</a>
-					</li>
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#347d39] ">
-						<a
-							className="flex justify-between items-center w-full text-gray-300"
-							href="https://cvthibautraimond.netlify.app/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							CV <AiOutlineProfile size={30} />
-						</a>
-					</li>
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ea4335]">
-						<Link
-						className="flex justify-between items-center w-full text-gray-300"
-							to="/portfolio-tailwind-react/contact"
-							mooth={true}
-							duration={500}
-						>
-							{" "}
-							Email{" "}
-						<HiOutlineMail size={30} />
-						</Link>{" "}
-					</li>
-				</ul>
-			</div>
 		</div>
 	);
 };
